e84ceb4110236c9d3af1b39fec05dfa5f453d0eb
ConstantinCezB <38817297+ConstantinCezB@users.noreply.github.com>
2018-09-17 22:53:57 -0400

    Design Update

---
+++ app/src/main/java/com/example/mwt/dateprogressdb/dateProgressEntity.kt
@@ -0,0 +1,14 @@
+package com.example.mwt.dateprogressdb
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dateProgress")
+data class dateProgressEntity(
+        val date: String,
+        val progress: Int
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0
+        protected set
+}
