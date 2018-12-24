d79bc95df2b1aabf01fc2809d3c7b63dad80a225
constantin <Constantin.Cez.b@gmail.com>
2018-12-24 13:48:49 -0500

    WorkManager now saves the date progress objects in the database daily.

--- app/src/main/java/com/example/mwt/TrackerWorkerManager.kt
+++ app/src/main/java/com/example/mwt/TrackerWorkerManager.kt
@@ -3,27 +3,41 @@ package com.example.mwt
 import android.content.Context
 import android.content.SharedPreferences
 import android.util.Log
+import androidx.room.Room
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.example.mwt.db.MWTDatabase
+import com.example.mwt.db.MWTDatabase_Impl
+import com.example.mwt.db.dateprogressdb.DateProgressDao
+import com.example.mwt.db.dateprogressdb.DateProgressEntity
 import com.example.mwt.livedata.setInt
 import com.example.mwt.util.*
 import java.util.*
 
 class TrackerWorkerManager(context : Context, params : WorkerParameters) : Worker (context, params) {
 
+    val db = Room.databaseBuilder(
+            applicationContext,
+            MWTDatabase::class.java, "MyWaterTrackerDatabase.db"
+    ).build() //TODO: use the injected
+
     override fun doWork(): Result {
+
         val calendar = Calendar.getInstance()
         val preference: SharedPreferences = applicationContext.getSharedPreferences(SHARED_PREFERENCE_FILE, Context.MODE_PRIVATE)
         val previousDate: String = preference.getString(TIME_INTERVAL_PREVIOUS_WORKER_DATE, DEFAULT_INTERVAL_PREVIOUS_WORKER_DATE)!!
+        val currentDate: String = calendar.getDate()
 
-        Log.d("TEST", calendar.getDate())
+        Log.d("TEST", currentDate)
 
-        if(previousDate != DEFAULT_INTERVAL_PREVIOUS_WORKER_DATE && previousDate != calendar.getDate()){
+        if(previousDate != DEFAULT_INTERVAL_PREVIOUS_WORKER_DATE && previousDate != currentDate){
             val numerator = preference.getInt(SHARED_PREFERENCE_NUMERATOR_DAILY, DEFAULT_NUMERATOR)
             preference.setInt(SHARED_PREFERENCE_NUMERATOR_DAILY, 0)
-            preference.edit().putString(TIME_INTERVAL_PREVIOUS_WORKER_DATE, calendar.getDate()).apply()
+            preference.edit().putString(TIME_INTERVAL_PREVIOUS_WORKER_DATE, currentDate).apply()
+            db.dateProgressDao().save(DateProgressEntity(previousDate, numerator))
+
         } else {
-            preference.edit().putString(TIME_INTERVAL_PREVIOUS_WORKER_DATE, calendar.getDate()).apply()
+            preference.edit().putString(TIME_INTERVAL_PREVIOUS_WORKER_DATE, currentDate).apply()
         }
 
         return Result.success()
--- app/src/main/java/com/example/mwt/db/dateprogressdb/DateProgressEntity.kt
+++ app/src/main/java/com/example/mwt/db/dateprogressdb/DateProgressEntity.kt
@@ -6,8 +6,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "dateProgress")
 data class DateProgressEntity(
         val date: String,
-        val progress: Int,
-        val dayString: String
+        val progress: Int
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0
