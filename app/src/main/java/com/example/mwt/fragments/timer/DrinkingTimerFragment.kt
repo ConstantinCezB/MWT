92fecf40f0253074672e0fb82a4b3b29436305ff
constantin <Constantin.Cez.b@gmail.com>
2019-01-10 09:00:11 -0500

    added text inside recycler (for the daily log) to show if it is empty or not. Refactoring had been done.

--- app/src/main/java/com/example/mwt/fragments/timer/DrinkingTimerFragment.kt
+++ app/src/main/java/com/example/mwt/fragments/timer/DrinkingTimerFragment.kt
@@ -1,6 +1,7 @@
 package com.example.mwt.fragments.timer
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mwt.R
 import com.example.mwt.recyclerview.ContainerLogRecyclerViewAdapter
+import kotlinx.android.synthetic.main.drinking_timer_fragment.*
 import kotlinx.android.synthetic.main.drinking_timer_fragment.view.*
 import kotlinx.android.synthetic.main.tracker_fragment.*
 import org.koin.android.viewmodel.ext.android.getViewModel
@@ -25,15 +27,23 @@ class DrinkingTimerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.recyclerFavoriteContainerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        view.recyclerDailyLogView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = getViewModel()
 
-        containerDrankRecyclerViewAdapter = ContainerLogRecyclerViewAdapter().also(recyclerFavoriteContainerView::setAdapter)
+        containerDrankRecyclerViewAdapter = ContainerLogRecyclerViewAdapter().also(recyclerDailyLogView::setAdapter)
 
-        viewModel.getAllPosts().observe(viewLifecycleOwner, Observer(containerDrankRecyclerViewAdapter::submitList))
+        viewModel.getAllPosts().observe(viewLifecycleOwner, Observer {
+            containerDrankRecyclerViewAdapter.submitList(it)
+            showEmptyLogDisplay(containerDrankRecyclerViewAdapter.itemCount)
+        })
+    }
+
+    private fun showEmptyLogDisplay (adapterSize: Int){
+        if (adapterSize == 0) display_no_log.visibility = View.VISIBLE
+        else display_no_log.visibility = View.GONE
     }
 }
