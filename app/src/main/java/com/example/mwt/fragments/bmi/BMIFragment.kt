7e138c3687ba828b7e2f7f035d3ed789cf147434
Constantin Cezar Begu <38817297+ConstantinCezB@users.noreply.github.com>
2019-01-15 10:07:04 -0500

    created date picker dialog and the constant values that are going to go in the shared pref

--- app/src/main/java/com/example/mwt/fragments/bmi/BMIFragment.kt
+++ app/src/main/java/com/example/mwt/fragments/bmi/BMIFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mwt.fragments.bmi
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import com.example.mwt.R
 import com.example.mwt.util.showContent
 import kotlinx.android.synthetic.main.bmi_fragment.view.*
+import java.util.*
 
 class BMIFragment: Fragment() {
 
@@ -19,6 +21,8 @@ class BMIFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val mDateListenor =
 
         view.constraintLayoutInput.setOnClickListener {
             view.constraintLayoutInputToDrop.showContent(view.bmi_input_edit_drop)
@@ -35,10 +39,24 @@ class BMIFragment: Fragment() {
         view.constraintLayoutLog.setOnClickListener {
             view.constraintLayoutLogToDrop.showContent(view.bmi_log_edit_drop)
         }
+
+        view.datePickerDialogPrompt.setOnClickListener {
+            showDateDialog()
+        }
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+    }
+
+    private fun showDateDialog () {
+        val calendar = Calendar.getInstance()
+        val dialog = DatePickerDialog(context!!, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+        }, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
+
+        dialog.show()
     }
 
 
--- app/src/main/java/com/example/mwt/util/Constants.kt
+++ app/src/main/java/com/example/mwt/util/Constants.kt
@@ -1,11 +1,23 @@
 package com.example.mwt.util
 
+import com.example.mwt.R
+
+//constant for the shared pref file
 const val SHARED_PREFERENCE_FILE = "com.constantin.pref"
+
+//unique worker name
+const val UNIQUE_WORKER_NAME_TRACKER = "workerTrackerPeriodic"
+
+//date variables for the daily water that the user drank
 const val SHARED_PREFERENCE_NUMERATOR_DAILY = "numerator"
 const val SHARED_PREFERENCE_DENOMINATOR_DAILY = "denominator"
-const val UNIQUE_WORKER_NAME_TRACKER = "workerTrackerPeriodic"
 const val TIME_INTERVAL_PREVIOUS_WORKER_DATE = "previousWorkerTimeInterval"
 
 const val DEFAULT_NUMERATOR = 0
 const val DEFAULT_DENOMINATOR = 2000
 const val DEFAULT_INTERVAL_PREVIOUS_WORKER_DATE = "NULL"
+
+//variables for the input tab in the bmi
+const val SHARED_PREFERENCE_DATE_OF_BIRTH = "DateOfBirth"
+const val DEFAULT_DATE = "NULL"
+
