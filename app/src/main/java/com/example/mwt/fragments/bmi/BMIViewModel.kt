c29577e592a3a38b0f17e89ff44f609c9d54cf2e
ConstantinCezB <Constantin.Cez.b@gmail.com>
2019-04-29 13:10:35 -0400

    Created the BMI view Model and made it work with the database.

---
+++ app/src/main/java/com/example/mwt/fragments/bmi/BMIViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.mwt.fragments.bmi
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.mwt.db.bmiRecordsdb.BMIRecordDao
+import com.example.mwt.db.bmiRecordsdb.BMIRecordEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class BMIViewModel(private val bmiRecordDao: BMIRecordDao) : ViewModel(), CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Default
+
+
+    fun getAllPosts(): LiveData<List<BMIRecordEntity>> {
+        return bmiRecordDao.findAll()
+    }
+
+    fun savePost(bmiRecordEntity: BMIRecordEntity) {
+        launch{ bmiRecordDao.save(bmiRecordEntity) }
+    }
+
+    fun deletePost(bmiRecordEntity: BMIRecordEntity) {
+        launch{ bmiRecordDao.delete(bmiRecordEntity) }
+    }
+}
